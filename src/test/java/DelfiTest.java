3b0b4bfa4eed8d6380413a10fb48dd0b04f0c5bd
Tanya Dabolina <lisica@inbox.lv>
2017-10-08 07:54:10 +0300

    updated for delfi test 08/10

--- src/test/java/DelfiTest.java
+++ src/test/java/DelfiTest.java
@@ -1,106 +1,138 @@
-//import org.junit.Test;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class DelfiTest {
-//    private String HOME_PAGE = "http://www.delfi.lv";
-//    private String HOME_PAGE2 = "http://www.mobiledelfi.lv";
-////    private static final By ARTICLE = By.xpath("")");
-////    private static final By ARTICLE_TITLE = By.xpath("")");
-////    private static final By INSIDE_TITLE = driver.findElement(By.xpath("./("");
-//
-////    import java.unit.ArticeList;
-////    import java.unit.List;
-///*
-//Delfi.lv testing
-//
-//1. go to defi.lv
-//a. go to mobile mobile delfi.lv
-//2. find 5 first titles (by xPath)
-//3. record in List titles, comments count
-//. create 2 Lists:
-//. ArrayListe<String> titles,
-//ArrayList<Integer> count
-//for(i=0;i<5;i++)
-//substring
-//intager value off
-//
-//4. go to first title
-//5. verify title , verify comments count
-//6. go to comments tab
-//7. verify title
-//8. count total comments:
-//record zareg. Comments
-//record anonim Comments
-//9. verify comments
-//10. repeat step 3 -9 for all 5 titles
-//
-//dobavlenie
-//sozdat peremennuju kotoroj mozhno budet prisvoit nazvanie statji i sravnit comment count v obichnoj versii i v mobilnoj
-//*/
-//
+import firstTest.FirstTestTest;
+import jdk.nashorn.internal.runtime.options.LoggingOption;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DelfiTest {
+    private String HOME_PAGE = "http://www.delfi.lv";
+    private String HOME_PAGE2 = "http://www.mobiledelfi.lv";
+    private static final By ARTICLE_FULL = By.xpath ("(//a[@class='top2012-title'])[1]");
+    //    private static final By ARTICLE = By.xpath("")");
+//    private static final By ARTICLE_TITLE = By.xpath("")");
+//    private static final By INSIDE_TITLE = driver.findElement(By.xpath("./("");
+    String TITLE;
+    String TITLE_COUNT;
+    private static final Logger LOGGER = LogManager.getLogger(FirstTestTest.class);
+
+//    import java.unit.ArticeList;
+//    import java.unit.List;
+/*
+Delfi.lv testing
+
+1. go to defi.lv
+a. go to mobile mobile delfi.lv
+2. find 5 first titles (by xPath)
+3. record in List titles, comments count
+. create 2 Lists:
+. ArrayListe<String> titles,
+ArrayList<Integer> count
+for(i=0;i<5;i++)
+substring
+intager value off
+
+4. go to first title
+5. verify title , verify comments count
+6. go to comments tab
+7. verify title
+8. count total comments:
+record zareg. Comments
+record anonim Comments
+9. verify comments
+10. repeat step 3 -9 for all 5 titles
+
+dobavlenie
+sozdat peremennuju kotoroj mozhno budet prisvoit nazvanie statji i sravnit comment count v obichnoj versii i v mobilnoj
+*/
+
+    @Test
+    public void DelfiMainPage() throws InterruptedException {
+        System.setProperty ("webdriver.gecko.driver", "C:/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver ();
+        //WebDriver mdriver = new FirefoxDriver();
+//        List<String> ARTICLE_COMMENT_COUNT=List<String>
+        WebDriverWait wait = new WebDriverWait (driver, 10);
+        driver.get (HOME_PAGE);
+        driver.manage ().window ().maximize ();
+        // mdriver.get(HOME_PAGE2);
+
+
+//        List<WebElement>articeList = driver.findElements(ARTICLE);
+
+        List<String> TITLES = new ArrayList<String> ();
+        List<Integer> COUNT = new ArrayList<Integer> ();
+        List<Long> IDS_TO_CHECK = new ArrayList<Long> ();
+
+//        ACTUAL_NAME_ERROR= driver.findElement(By.xpath("(//div[@class='required-message'])[1]")).getText();
+        TITLE = driver.findElement(By.xpath ("(//a[@class='top2012-title'])[1]")).getText();
+
+        TITLE_COUNT = driver.findElement(By.xpath ("(//a[@class='comment-count'])")).getText();//schitivaetsa kolichstvo kommentariev v pervoj statje
+        System.out.println ("TITLE is: "+ TITLE +" comments:"+ TITLE_COUNT);//vivioditsa nazvanie statji i kollichestvo komentov k nej
+
+        List<WebElement>art_icle_count = driver.findElements(By.xpath ("(//a[@class='top2012-title'])"));// sozdajom spisok i zapisivaem tuda nazvanie statji
+
+        ArrayList<String> article_title = new ArrayList<String>(5); // sozdajom massiv s kollichestvom stork =5
+//        (elements.size();
+
+
+        System.out.println("Title count:"+ art_icle_count.size()); // size -schitaet kollichestvo statej na stranice vsego
+
+        for(int i=0;i<5;i++){        //narashivaet masiv do 5 (t.k kolichstvo statej nas ne interesuet)
+
+            TITLE = driver.findElements (By.xpath ("(//a[@class='top2012-title'])")).get (i).getText ();
+            article_title.add(TITLE);                  //zapihivaem v masiv nazvanie statji
+            System.out.println("Nomer article : "+i+" article Title : "+ article_title);
+//            System.out.println(art_icle_count.get(i).getText());
+        }
+        //Logger.info ("Vivodim konechnij massiv ");   //logger ne rabotaet ???
+        System.out.println("Vivodim konechnij massiv ");
+        System.out.println("article Titles : "+ article_title);
+
+/*
+        for(int i=0;i<5; i++) {
+            WebElement element = TITLES.get(i);
+            TITLES.add(element.findElement(ARTICLE_TITLE).getText());
+            IDS_TO_CHECK.add(element.findElement(ARTICLE_TITLE).getAttribute(s:"href").substring())); //berem nazvanie statji v vide id
+            // proveraem est li voobshe comenti
+            if (element.findElement(ARTICLE_COMMENT_COUNT).size() != 0) { //obrezaem skobki v komntarijah
+                String countToParse = element.findElement(ARTICLE_COMMENT_COUNT).getText(); //naprimer u nas (106) komentov
+                countToParse=countToParse.substring(countToParse.indexOf('('))+1, countToParse.indexOf(')')); // poluchem 106 (bez skobok)
+                count.add(Integer.valueOf(countToParse)); //peredelivaem sting value v integer
+            }
+            // esli comnetarive voobshe net to prisvaevaem zanchenie = 0
+        }else{
+            count.add(0);
+        }
+                    }
+
+}
+                    //kak perejti na addres iz lista
+//    driver.get("http://delfi.lv/delfi.lv/blablabla?id=" +idsToCheck.get(2));
+//// bezhim po spisku title
+//            for (int i=0; i<titles.size(); i++){
+//            Assert.assertEquals("Title is not equal for an article:", +title.get(i), mobileTitle.get(), titles.get(i),mobileTatles.)
+//Assert.assertEquals();
+*/
+/*
+
+        ActualTitle = driver.findElements(ARTICLE_TITLE).getText();
+        driver.findElement(ARTICLE_TITLE).click();
+        InsideTitle = driver.findElements(INSIDE_TITLE).getText();
+        Assert.assertEqual(ARTICLE_TITLE)
+*/
+//    }}
+
 //    @Test
-//    public void DelfiMainPage() throws InterruptedException {
-//        System.setProperty("webdriver.gecko.driver", "C:/geckodriver.exe");
-//        WebDriver driver = new FirefoxDriver();
-//        WebDriver mdriver = new FirefoxDriver();
-////        List<String> ARTICLE_COMMENT_COUNT=List<String>
-//        //WebDriverWait wait= new WebDriverWait(driver,10);
-//        driver.get(HOME_PAGE);
-//        mdriver.get(HOME_PAGE2);
+//public void secondTest()
+//        {
 //
-////        List<WebElement>articeList = driver.findElements(ARTICLE);
-//
-//        List<String> titles = new ArrayList<String>();
-//        List<Integer> count = new ArrayList<Integer>();
-//        List<Long>idsToCheck = new ArrayList<Long>();
-//
-//        //sozajom listi dlja mobilok
-//        List<String> mobileTitle = new ArrayList<String>();
-//        List<Integer> mobileCount = new ArrayList<Integer>();
-//        List<Long> midsToCheck = new ArrayList<Long>(); // id dolzhi bit odinakovie kak na obichnoj versii tak i na mobilnoj
-//
-//        for(int i=0;i<5; i++) {
-////            WebElement element = articleList.get(i);
-////            titles.add(element.findElement(ARTICLE_TITLE).getText());
-////            idsToCheck.add(element.findElement(ARTICLE_TITLE).getAttribute(s:"href").substring())); //berem nazvanie statji v vide id
-////            // proveraem est li voobshe comenti
-////            if (element.findElement(ARTICLE_COMMENT_COUNT).size() != 0) { //obrezaem skobki v komntarijah
-////                String countToParse = element.findElement(ARTICLE_COMMENT_COUNT).getText(); //naprimer u nas (106) komentov
-////                countToParse=countToParse.substring(countToParse.indexOf('('))+1, countToParse.indexOf(')')); // poluchem 106 (bez skobok)
-////                count.add(Integer.valueOf(countToParse)); //peredelivaem sting value v integer
-////            }
-//            // esli comnetarive voobshe net to prisvaevaem zanchenie = 0
-////        }else{
-////            count.add(0);
-////        }
-//                    }
-//
-//}
-//                    //kak perejti na addres iz lista
-////    driver.get("http://delfi.lv/delfi.lv/blablabla?id=" +idsToCheck.get(2));
-////// bezhim po spisku title
-////            for (int i=0; i<titles.size(); i++){
-////            Assert.assertEquals("Title is not equal for an article:", +title.get(i), mobileTitle.get(), titles.get(i),mobileTatles.)
-////Assert.assertEquals();
-//
-///*
-//
-//        ActualTitle = driver.findElements(ARTICLE_TITLE).getText();
-//        driver.findElement(ARTICLE_TITLE).click();
-//        InsideTitle = driver.findElements(INSIDE_TITLE).getText();
-//        Assert.assertEqual(ARTICLE_TITLE)
-//*/
-////    }}
-//
-////    @Test
-////public void secondTest()
-////        {
-////
-////        }
+    }
+}
--- src/test/java/firstTest/FirstTestTest.java
+++ src/test/java/firstTest/FirstTestTest.java
@@ -3,6 +3,8 @@ package firstTest;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -15,22 +17,26 @@ import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 
+
 public class FirstTestTest {
     private String HOME_PAGE = "http://javaguru.lv";
     private static final By QA_COURSE_MENU_LINK = By.xpath(".//span[text()='Курс Тестирования(QA)']");
     private static final By QA_REGISTRATION_BTN = By.className("regCourseA");
-    //private static final By ERROR_MSG = By.xpath("./div[text()='Šis ir obligāts jautājums.']");
     private static final By NAME_FIELD = By.id("entry_1000000");
     private static final By SURNAME =By.id("entry_1000001");
     private static final By MOB =By.id("entry_1000002") ;
     private static final By MAIL = By.id("entry_1000003");
-    //private static final By COMP_YN = By.id("group_738971122_1");
-    //private static final By COMP_YN = By.xpath("//div[contains(label(),'QA 2']");
     private static final By LAPTOP_YN = By.xpath("//input[contains(@class, 'ss-q-radio')]");
     private static final By QA_COURSE_12 = By.xpath("//input[contains(@id, 'group_1887084786_2')]");
     private static final By SEND_F = By.id("ss-submit");
-
-
+    private static final Logger LOGGER = LogManager.getLogger(FirstTestTest.class);
+    String EXP_ERROR="Šis ir obligāts jautājums.";
+    String ACTUAL_NAME_ERROR;
+    String ACTUAL_SURNAME_ERROR;
+    String ACTUAL_EMAIL_ERROR;
+    String ACTUAL_PHONE_ERROR;
+    String ACTUAL_COURSE_N_ERROR;
+    String ACTUAL_NOTE_ERROR;
 
     @Test
     public void FirstTest() throws InterruptedException {
@@ -41,16 +47,16 @@ public class FirstTestTest {
         driver.get(HOME_PAGE);
         driver.findElement(QA_COURSE_MENU_LINK).click();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+/*
 //       ---- obuchenie poiska po dvojnomu moasivu
         //poisk po HashMap
         HashMap<String, Integer> tmp = new HashMap<String, Integer>();
         Integer commentCount = tmp.get("Ushakov");
 
-        /*
+
         article.put("Ushakov", 30);
         Integer commentCount = article.get("Ushakov");
-        */
+
 
         //poisk po spisku
         List<String> titles = new ArrayList<String>();
@@ -66,7 +72,7 @@ public class FirstTestTest {
         }
 //      ------
 
-
+*/
         driver.findElement(QA_REGISTRATION_BTN).click();
         ArrayList<String> tabs = new ArrayList<String>();
         while (tabs.size() != 2) {
@@ -80,18 +86,45 @@ public class FirstTestTest {
 //        element.clear();
 //        element.sendKeys();
 
-//        mandatory fields check
+        LOGGER.info("We are starting to test mandatory atttibutes: ");
         driver.findElement(SEND_F).click();
 
-
         driver.findElement(By.className("ss-form-title")).click(); // mouse out of mandatory fields
+
+        //Assert.assertEquals("* Nepieciešams", source.getText());
+        // Assert.assertTrue(driver.findElement(NAME_ERROR).isDisplayed());
+        //Assert.assertTrue("Šis ir obligāts jautājums.", driver.findElement(NAME_ERROR)).isDisplayed());//Name
+        ACTUAL_NAME_ERROR= driver.findElement(By.xpath("(//div[@class='required-message'])[1]")).getText();
+        System.out.println("ACTUAL_NAME_ERROR:"+ ACTUAL_NAME_ERROR);
+        Assert.assertEquals(EXP_ERROR,ACTUAL_NAME_ERROR); //name
+        ACTUAL_SURNAME_ERROR= driver.findElement(By.xpath("(//div[@class='required-message'])[2]")).getText();
+        System.out.println("ACTUAL_SURNAME_ERROR:"+ ACTUAL_SURNAME_ERROR);
+        Assert.assertEquals(EXP_ERROR,ACTUAL_SURNAME_ERROR); //Surname
+        ACTUAL_PHONE_ERROR= driver.findElement(By.xpath("(//div[@class='required-message'])[3]")).getText();
+        System.out.println("ACTUAL_PHONE_ERROR:"+ ACTUAL_PHONE_ERROR);
+        Assert.assertEquals(EXP_ERROR,ACTUAL_PHONE_ERROR); //Phone
+        ACTUAL_EMAIL_ERROR= driver.findElement(By.xpath("(//div[@class='required-message'])[3]")).getText();
+        System.out.println("ACTUAL_EMAIL_ERROR:"+ ACTUAL_EMAIL_ERROR);
+        Assert.assertEquals(EXP_ERROR,ACTUAL_EMAIL_ERROR); //Email
+        ACTUAL_COURSE_N_ERROR= driver.findElement(By.xpath("(//div[@class='required-message'])[3]")).getText();
+        System.out.println("ACTUAL_COURSE_N_ERROR:"+ ACTUAL_COURSE_N_ERROR);
+        Assert.assertEquals(EXP_ERROR,ACTUAL_COURSE_N_ERROR); //course_2
+        ACTUAL_NOTE_ERROR= driver.findElement(By.xpath("(//div[@class='required-message'])[3]")).getText();
+        System.out.println("ACTUAL_EMAIL_ERROR:"+ ACTUAL_NOTE_ERROR);
+        Assert.assertEquals(EXP_ERROR,ACTUAL_NOTE_ERROR); //Note
+//
+//        Assert.assertTrue(driver.findElement(NAME_ERROR).isDisplayed());//Surname
+//        Assert.assertTrue("Šis ir obligāts jautājums.", driver.findElement(By.id("351984802_errorMessage")).isDisplayed());//Phone
+//        Assert.assertTrue("No error messages", driver.findElements(By.className("required-message" ).size()>0);
+/*
     if (driver.findElement(By.id("1953673096_errorMessage")).isDisplayed());
         {
             System.out.println("Šis ir obligāts jautājums.Name");
         }
+*/
 
+        LOGGER.info("We are starting to fill form ");
 
-//        fill form
         driver.findElement(NAME_FIELD).sendKeys("My Name");
         driver.findElement(SURNAME).sendKeys("My Surname");
         driver.findElement(MAIL).sendKeys("t4321@inbox.lv");
@@ -102,16 +135,16 @@ public class FirstTestTest {
 
 
         driver.findElement(SEND_F).click();
-//        Assert.assertEquals("* Nepieciešams", source.getText());
-//        Assert.assertTrue(driver.findElement(By.id("profile-page")).isDisplayed());
-//        Assert.assertTrue("No error messages", driver.findElements(ERROR_MSG ).size()>0);
+//          Assert.assertEquals("* Nepieciešams", source.getText());
+//          Assert.assertTrue("ja upalo", driver.findElement(By.id("profile-page")).isDisplayed());
+// !        Assert.assertTrue("Šis ir obligāts jautājums.", driver.findElement(By.xpath("(//div[@class='required-message'])[1]").getSize()>0);
+//          Assert.assertTrue("No error messages", driver.findElements(ERROR_MSG ).size()>0);
 
         if (driver.findElement(By.className("ss-confirmation")).isDisplayed());
         {
-            // Print a Log In message to the screen
             System.out.println("Your response has been recorded.");
         }
-
+        System.out.println("TEST Passed");
         driver.quit();
     }
 }
